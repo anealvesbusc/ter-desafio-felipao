fd05fc133afca30696a7f75cfcec36f8837ec92e
anealvesbusc <anysevla.ea@gmail.com>
2024-01-11 10:32:42 -0300

    Projeto 3

---
+++ index.js
@@ -0,0 +1,11 @@
+class heroiVantura {
+    constructor (nome, idade, ) {
+        this.nome = nome
+        this.idade = idade 
+        this.tipo = tipo   
+
+    }
+    
+}
+let tipoHeroi = new heroiVantura ("Vapira" , 37, "ninja" ) 
+console.log (tipoHeroi)                           
